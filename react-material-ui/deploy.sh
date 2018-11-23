aac58ae39cc61f24c59dab12e97b34cc11fcfe64
Rick Shea <rick@beachfront.com>
2018-11-23 09:21:45 -0500

    Foo

--- react-material-ui/deploy.sh
+++ react-material-ui/deploy.sh
@@ -4,16 +4,11 @@ BRANCH=${1:-master}
 
 GREEN=`tput -Txterm-256color setaf 2`
 
-# Change to working directory
-cd /opt/app/BidScout-Client 2> /dev/null
-
 # Checkout Branch
 echo "${GREEN}Checking out ${BRANCH} Branch${RESET}"
 git checkout ${BRANCH} 2> /dev/null
 
 git pull 2> /dev/null
-
-cd /react-material-ui 2> /dev/null
 
 yarn run build 2> /dev/null
 
