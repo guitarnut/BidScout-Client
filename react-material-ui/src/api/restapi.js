ba59b8764d43621b0e1ff785f874ff4ed14fd90b
Rick Shea <rick@beachfront.com>
2018-11-25 14:26:52 -0500

    W

--- react-material-ui/src/api/restapi.js
+++ react-material-ui/src/api/restapi.js
@@ -1,8 +1,8 @@
 import axios from "axios/index";
 
 const sessionTimeout = 1000 * 60 * 60;
-const host = 'http://localhost:8080';
-//const host = 'https://app.auctionscout.net';
+//const host = 'http://localhost:8080';
+const host = 'https://app.auctionscout.net';
 
 let session = null;
 
