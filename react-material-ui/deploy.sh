f4c528d5868b2924689ebad672faa0d3d77ad1e4
Rick Shea <rick@beachfront.com>
2018-12-11 11:21:23 -0500

    Deploy

--- react-material-ui/deploy.sh
+++ react-material-ui/deploy.sh
@@ -12,4 +12,19 @@ git pull 2> /dev/null
 
 yarn run build 2> /dev/null
 
+# Stop the Service (might not be running)
+echo "${GREEN}Stopping Service{RESET}"
+
+pid=$(pgrep -f 'node /usr/local/bin/serve -s build -p 80')
+
+if [ -n "$pid" ]; then
+    kill $pid
+    echo "Terminated process"
+else
+    echo "Does not exist"
+fi
+
+# Pause
+sleep 5
+
 nohup serve -s build -p 80&
