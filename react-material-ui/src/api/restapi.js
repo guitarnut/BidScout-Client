c6119608fcaa5414f2471791ce9af4051947d8a1
Rick Shea <rick@beachfront.com>
2019-01-15 15:17:50 -0500

    Creatives fix

--- react-material-ui/src/api/restapi.js
+++ react-material-ui/src/api/restapi.js
@@ -1,7 +1,7 @@
 import axios from "axios/index";
 
 export const sessionTimeout = 1000 * 60 * 60;
-export const host = 'http://localhost:8080'; //'https://app.auctionscout.net'; //process.env.REACT_APP_HOST;
+export const host = 'https://app.auctionscout.net'; //process.env.REACT_APP_HOST;
 
 let session = null;
 
